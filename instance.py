b9420e64c9fd871dc05cb9a958b4c6c2e08af9f5
caiqian <zero530@126.com>
2024-04-10 14:24:36 +0800

    成功使用driver, 连接到已有实例

--- instance.py
+++ instance.py
@@ -1,11 +1,23 @@
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()
-chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9515")
-# chrome_driver_path = "D:/data/chrome/chrome-win64/chrome-win64/chrome.exe"
-chrome_driver_path = "D:/data/chrome/chromedriver_win32/chromedriver.exe"
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
-# driver = webdriver.Chrome(options=chrome_options)
-driver = webdriver.Chrome(service=Service(chrome_driver_path), options=chrome_options)
+# 指定ChromeDriver路径
+chrome_driver_path = 'D:/data/chrome/chromedriver-win64/chromedriver-win64/chromedriver.exe'
+
+# 设置Service
+service = Service(executable_path=chrome_driver_path)
+
+# 创建WebDriver对象
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+# 现在你可以控制已打开的浏览器实例了
+# driver.get("https://www.baidu.com")
+driver.get("https://www.douyin.com/")
+
+sleep(5)
