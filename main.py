5b84a07712f13a0f9f3933968a53314ab27194c3
caiqian <zero530@126.com>
2024-04-12 11:20:21 +0800

    add count para

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from utils.argument_parser import parse_arguments
 
 if __name__ == '__main__':
+    max_count = 200
     args = parse_arguments()
     print(f"input url: {args.url}")
+    if args.count:
+        print(f"input count: {args.count}")
+        max_count = int(args.count)
+    else:
+        print("no count input,will use default value 200")
--- utils/argument_parser.py
+++ utils/argument_parser.py
@@ -4,7 +4,10 @@ import argparse
 def parse_arguments():
     parser = argparse.ArgumentParser(description='请提供视频地址')
 
-    # 添加命令行参数
+    # add url argument
     parser.add_argument('--url', help='url地址')
+
+    # add max count argument
+    parser.add_argument('--count', help='最大关注次数')
 
     return parser.parse_args()
